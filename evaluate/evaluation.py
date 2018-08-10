76307f51398be3060b412346e70d64f106ed315e
tensorboy <tensorboy@aifi.io>
2018-08-09 17:03:17 -0700

    update readme for evaluation

--- evaluate/evaluation.py
+++ evaluate/evaluation.py
@@ -4,6 +4,7 @@ from evaluate.coco_eval import run_eval
 from network.rtpose_vgg import get_model, use_vgg
 from torch import load
 
+#Notice, if you using the 
 with torch.autograd.no_grad():
     # this path is with respect to the root of the project
     weight_name = './network/weight/best_pose.pth'
@@ -17,8 +18,8 @@ with torch.autograd.no_grad():
     model = model.cuda()
     
     # The choice of image preprocessing include: 'rtpose', 'inception', 'vgg' and 'ssd'.
-    # 'our_post_processing' using the ported Matlab post processing, which got slightly higher score
-    # else using the ported C++ post-processing, which has amazing speed.
+    # If you use the converted model from caffe, it is 'rtpose' preprocess, the model trained in 
+    # this repo used 'vgg' preprocess
     run_eval(image_dir= '/data/coco/images/', anno_dir = '/data/coco', vis_dir = '/data/coco/vis',
         image_list_txt='./evaluate/image_info_val2014_1k.txt', 
         model=model, preprocess='vgg')
